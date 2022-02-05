c0aef9eff98c5cd4adde574aa79789ccfa30e870
andr00 <andr00@gmail.com>
2022-02-04 18:33:59 -0800

    Code format with black

--- download_comments.py
+++ download_comments.py
@@ -9,10 +9,10 @@ from auth import cookies, headers
 
 def fetch_xml(params):
     response = requests.get(
-        'https://www.livejournal.com/export_comments.bml',
+        "https://www.livejournal.com/export_comments.bml",
         params=params,
         headers=headers,
-        cookies=cookies
+        cookies=cookies,
     )
 
     return response.text
@@ -21,10 +21,10 @@ def fetch_xml(params):
 def get_users_map(xml):
     users = {}
 
-    for user in xml.iter('usermap'):
-        users[user.attrib['id']] = user.attrib['user']
+    for user in xml.iter("usermap"):
+        users[user.attrib["id"]] = user.attrib["user"]
 
-    with open('comments-json/usermap.json', 'w', encoding='utf-8') as f:
+    with open("comments-json/usermap.json", "w", encoding="utf-8") as f:
         f.write(json.dumps(users, ensure_ascii=False, indent=2))
 
     return users
@@ -45,40 +45,42 @@ def get_more_comments(start_id, users):
     comments = []
     local_max_id = -1
 
-    xml = fetch_xml({'get': 'comment_body', 'startid': start_id})
-    with open('comments-xml/comment_body-{0}.xml'.format(start_id), 'w', encoding='utf-8') as f:
+    xml = fetch_xml({"get": "comment_body", "startid": start_id})
+    with open(
+        "comments-xml/comment_body-{0}.xml".format(start_id), "w", encoding="utf-8"
+    ) as f:
         f.write(xml)
 
-    for comment_xml in ET.fromstring(xml).iter('comment'):
+    for comment_xml in ET.fromstring(xml).iter("comment"):
         comment = {
-            'jitemid': int(comment_xml.attrib['jitemid']),
-            'id': int(comment_xml.attrib['id']),
-            'children': []
+            "jitemid": int(comment_xml.attrib["jitemid"]),
+            "id": int(comment_xml.attrib["id"]),
+            "children": [],
         }
-        get_comment_property('parentid', comment_xml, comment)
-        get_comment_property('posterid', comment_xml, comment)
-        get_comment_element('date', comment_xml, comment)
-        get_comment_element('subject', comment_xml, comment)
-        get_comment_element('body', comment_xml, comment)
+        get_comment_property("parentid", comment_xml, comment)
+        get_comment_property("posterid", comment_xml, comment)
+        get_comment_element("date", comment_xml, comment)
+        get_comment_element("subject", comment_xml, comment)
+        get_comment_element("body", comment_xml, comment)
 
-        if 'state' in comment_xml.attrib:
-            comment['state'] = comment_xml.attrib['state']
+        if "state" in comment_xml.attrib:
+            comment["state"] = comment_xml.attrib["state"]
 
-        if 'posterid' in comment:
-            comment['author'] = users.get(str(comment['posterid']), "deleted-user")
+        if "posterid" in comment:
+            comment["author"] = users.get(str(comment["posterid"]), "deleted-user")
 
-        local_max_id = max(local_max_id, comment['id'])
+        local_max_id = max(local_max_id, comment["id"])
         comments.append(comment)
 
     return local_max_id, comments
 
 
 def download_comments():
-    os.makedirs('comments-xml', exist_ok=True)
-    os.makedirs('comments-json', exist_ok=True)
+    os.makedirs("comments-xml", exist_ok=True)
+    os.makedirs("comments-json", exist_ok=True)
 
-    metadata_xml = fetch_xml({'get': 'comment_meta', 'startid': 0})
-    with open('comments-xml/comment_meta.xml', 'w', encoding='utf-8') as f:
+    metadata_xml = fetch_xml({"get": "comment_meta", "startid": 0})
+    with open("comments-xml/comment_meta.xml", "w", encoding="utf-8") as f:
         f.write(metadata_xml)
 
     metadata = ET.fromstring(metadata_xml)
@@ -86,17 +88,16 @@ def download_comments():
 
     all_comments = []
     start_id = 0
-    max_id = int(metadata.find('maxid').text)
+    max_id = int(metadata.find("maxid").text)
     while start_id < max_id:
         start_id, comments = get_more_comments(start_id + 1, users)
         all_comments.extend(comments)
 
-    with open('comments-json/all.json', 'w', encoding='utf-8') as f:
+    with open("comments-json/all.json", "w", encoding="utf-8") as f:
         f.write(json.dumps(all_comments, ensure_ascii=False, indent=2))
 
     return all_comments
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     download_comments()
-    
--- export.py
+++ export.py
@@ -13,10 +13,10 @@ from download_posts import download_posts
 from download_comments import download_comments
 
 
-TAG = re.compile(r'\[!\[(.*?)\]\(http:\/\/utx.ambience.ru\/img\/.*?\)\]\(.*?\)')
+TAG = re.compile(r"\[!\[(.*?)\]\(http:\/\/utx.ambience.ru\/img\/.*?\)\]\(.*?\)")
 USER = re.compile(r'<lj user="?(.*?)"?>')
-TAGLESS_NEWLINES = re.compile('(?<!>)\n')
-NEWLINES = re.compile('(\s*\n){3,}')
+TAGLESS_NEWLINES = re.compile("(?<!>)\n")
+NEWLINES = re.compile("(\s*\n){3,}")
 
 SLUGS = {}
 
@@ -24,12 +24,12 @@ SLUGS = {}
 
 
 def fix_user_links(json):
-    """ replace user links with usernames """
-    if 'subject' in json:
-        json['subject'] = USER.sub(r'\1', json['subject'])
+    """replace user links with usernames"""
+    if "subject" in json:
+        json["subject"] = USER.sub(r"\1", json["subject"])
 
-    if 'body' in json:
-        json['body'] = USER.sub(r'\1', json['body'])
+    if "body" in json:
+        json["body"] = USER.sub(r"\1", json["body"])
 
 
 def json_to_html(json):
@@ -41,24 +41,24 @@ def json_to_html(json):
 {body}
 </article>
 """.format(
-        subject=json['subject'] or json['date'],
-        body=TAGLESS_NEWLINES.sub('<br>\n', json['body'])
+        subject=json["subject"] or json["date"],
+        body=TAGLESS_NEWLINES.sub("<br>\n", json["body"]),
     )
 
 
 def get_slug(json):
-    slug = json['subject']
+    slug = json["subject"]
     if not len(slug):
-        slug = json['id']
+        slug = json["id"]
 
-    if '<' in slug or '&' in slug:
-        slug = BeautifulSoup('<p>{0}</p>'.format(slug)).text
+    if "<" in slug or "&" in slug:
+        slug = BeautifulSoup("<p>{0}</p>".format(slug)).text
 
-    slug = re.compile(r'\W+').sub('-', slug)
-    slug = re.compile(r'^-|-$').sub('', slug)
+    slug = re.compile(r"\W+").sub("-", slug)
+    slug = re.compile(r"^-|-$").sub("", slug)
 
     if slug in SLUGS:
-        slug += (len(slug) and '-' or '') + json['id']
+        slug += (len(slug) and "-" or "") + json["id"]
 
     SLUGS[slug] = True
 
@@ -66,23 +66,23 @@ def get_slug(json):
 
 
 def json_to_markdown(json):
-    body = TAGLESS_NEWLINES.sub('<br>', json['body'])
+    body = TAGLESS_NEWLINES.sub("<br>", json["body"])
 
     h = html2text.HTML2Text()
     h.body_width = 0
     h.unicode_snob = True
     body = h.handle(body)
-    body = NEWLINES.sub('\n\n', body)
+    body = NEWLINES.sub("\n\n", body)
 
     # read UTX tags
     tags = TAG.findall(body)
-    json['tags'] = len(tags) and '\ntags: {0}'.format(', '.join(tags)) or ''
+    json["tags"] = len(tags) and "\ntags: {0}".format(", ".join(tags)) or ""
 
     # remove UTX tags from text
-    json['body'] = TAG.sub('', body).strip()
+    json["body"] = TAG.sub("", body).strip()
 
-    json['slug'] = get_slug(json)
-    json['subject'] = json['subject'] or json['date']
+    json["slug"] = get_slug(json)
+    json["subject"] = json["subject"] or json["date"]
 
     return """id: {id}
 title: {subject}
@@ -90,20 +90,22 @@ slug: {slug}
 date: {date}{tags}
 
 {body}
-""".format(**json)
+""".format(
+        **json
+    )
 
 
 def group_comments_by_post(comments):
     posts = {}
 
     for comment in comments:
-        post_id = comment['jitemid']
+        post_id = comment["jitemid"]
 
         if post_id not in posts:
             posts[post_id] = {}
 
         post = posts[post_id]
-        post[comment['id']] = comment
+        post[comment["id"]] = comment
 
     return posts
 
@@ -114,74 +116,88 @@ def nest_comments(comments):
     for comment in comments.values():
         fix_user_links(comment)
 
-        if 'parentid' not in comment:
+        if "parentid" not in comment:
             post.append(comment)
         else:
-            comments[comment['parentid']]['children'].append(comment)
+            comments[comment["parentid"]]["children"].append(comment)
 
     return post
 
 
 def comment_to_li(comment):
-    if 'state' in comment and comment['state'] == 'D':
-        return ''
+    if "state" in comment and comment["state"] == "D":
+        return ""
 
-    html = '<h3>{0}: {1}</h3>'.format(comment.get('author', 'anonym'), comment.get('subject', ''))
-    html += '\n<a id="comment-{0}"></a>'.format(comment['id'])
+    html = "<h3>{0}: {1}</h3>".format(
+        comment.get("author", "anonym"), comment.get("subject", "")
+    )
+    html += '\n<a id="comment-{0}"></a>'.format(comment["id"])
 
-    if 'body' in comment:
-        html += '\n' + markdown.markdown(TAGLESS_NEWLINES.sub('<br>\n', htmlescape(comment['body'])))
+    if "body" in comment:
+        html += "\n" + markdown.markdown(
+            TAGLESS_NEWLINES.sub("<br>\n", htmlescape(comment["body"]))
+        )
 
-    if len(comment['children']) > 0:
-        html += '\n' + comments_to_html(comment['children'])
+    if len(comment["children"]) > 0:
+        html += "\n" + comments_to_html(comment["children"])
 
-    subject_class = 'subject' in comment and ' class=subject' or ''
-    return '<li{0}>{1}\n</li>'.format(subject_class, html)
+    subject_class = "subject" in comment and " class=subject" or ""
+    return "<li{0}>{1}\n</li>".format(subject_class, html)
 
 
 def comments_to_html(comments):
-    return '<ul>\n{0}\n</ul>'.format('\n'.join(map(comment_to_li, sorted(comments, key=itemgetter('id')))))
+    return "<ul>\n{0}\n</ul>".format(
+        "\n".join(map(comment_to_li, sorted(comments, key=itemgetter("id"))))
+    )
 
 
 def save_as_json(id, json_post, post_comments):
-    json_data = {'id': id, 'post': json_post, 'comments': post_comments}
-    with open('posts-json/{0}.json'.format(id), 'w', encoding='utf-8') as f:
+    json_data = {"id": id, "post": json_post, "comments": post_comments}
+    with open("posts-json/{0}.json".format(id), "w", encoding="utf-8") as f:
         f.write(json.dumps(json_data, ensure_ascii=False, indent=2))
 
 
 def save_as_markdown(id, subfolder, json_post, post_comments_html):
-    os.makedirs('posts-markdown/{0}'.format(subfolder), exist_ok=True)
-    with open('posts-markdown/{0}/{1}.md'.format(subfolder, id), 'w', encoding='utf-8') as f:
+    os.makedirs("posts-markdown/{0}".format(subfolder), exist_ok=True)
+    with open(
+        "posts-markdown/{0}/{1}.md".format(subfolder, id), "w", encoding="utf-8"
+    ) as f:
         f.write(json_to_markdown(json_post))
     if post_comments_html:
-        with open('comments-markdown/{0}.md'.format(json_post['slug']), 'w', encoding='utf-8') as f:
+        with open(
+            "comments-markdown/{0}.md".format(json_post["slug"]), "w", encoding="utf-8"
+        ) as f:
             f.write(post_comments_html)
 
 
 def save_as_html(id, subfolder, json_post, post_comments_html):
-    os.makedirs('posts-html/{0}'.format(subfolder), exist_ok=True)
-    with open('posts-html/{0}/{1}.html'.format(subfolder, id), 'w', encoding='utf-8') as f:
+    os.makedirs("posts-html/{0}".format(subfolder), exist_ok=True)
+    with open(
+        "posts-html/{0}/{1}.html".format(subfolder, id), "w", encoding="utf-8"
+    ) as f:
         f.writelines(json_to_html(json_post))
         if post_comments_html:
-            f.write('\n<h2>Комментарии</h2>\n' + post_comments_html)
+            f.write("\n<h2>Комментарии</h2>\n" + post_comments_html)
 
 
 def combine(all_posts, all_comments):
-    os.makedirs('posts-html', exist_ok=True)
-    os.makedirs('posts-markdown', exist_ok=True)
-    os.makedirs('comments-markdown', exist_ok=True)
+    os.makedirs("posts-html", exist_ok=True)
+    os.makedirs("posts-markdown", exist_ok=True)
+    os.makedirs("comments-markdown", exist_ok=True)
 
     posts_comments = group_comments_by_post(all_comments)
 
     for json_post in all_posts:
-        id = json_post['id']
+        id = json_post["id"]
         jitemid = int(id) >> 8
 
-        date = datetime.strptime(json_post['date'], '%Y-%m-%d %H:%M:%S')
-        subfolder = '{0.year}-{0.month:02d}'.format(date)
+        date = datetime.strptime(json_post["date"], "%Y-%m-%d %H:%M:%S")
+        subfolder = "{0.year}-{0.month:02d}".format(date)
 
-        post_comments = jitemid in posts_comments and nest_comments(posts_comments[jitemid]) or None
-        post_comments_html = post_comments and comments_to_html(post_comments) or ''
+        post_comments = (
+            jitemid in posts_comments and nest_comments(posts_comments[jitemid]) or None
+        )
+        post_comments_html = post_comments and comments_to_html(post_comments) or ""
 
         fix_user_links(json_post)
 
@@ -190,15 +206,15 @@ def combine(all_posts, all_comments):
         save_as_markdown(id, subfolder, json_post, post_comments_html)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if True:
         all_posts = download_posts()
         all_comments = download_comments()
 
     else:
-        with open('posts-json/all.json', 'r', encoding='utf-8') as f:
+        with open("posts-json/all.json", "r", encoding="utf-8") as f:
             all_posts = json.load(f)
-        with open('comments-json/all.json', 'r', encoding='utf-8') as f:
+        with open("comments-json/all.json", "r", encoding="utf-8") as f:
             all_comments = json.load(f)
 
     combine(all_posts, all_comments)
